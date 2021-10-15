c4e8c276498baba2c3af60b0fe7eba6b53a8ca50
Luffy Chen <luffychen0715@gmail.com>
2021-10-15 12:56:01 +0800

    feat: add Intro and signup reminder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import Intro from './components/Intro';
 import Speaker from './components/Speaker';
 import Agenda from './components/Agenda';
 // import Footer from './components/Footer/Footer';
@@ -14,6 +15,15 @@ const App = () => {
     <div className='App'>
       <Header></Header>
       <div className='container'>
+        <section>
+          <Intro></Intro>
+        </section>
+        <section>
+          <h2>報名資訊與連結</h2>
+          <p className='signup-reminder'>
+            預計10/25開放報名，報名人數因疫情將分A、B場報名，各有人數限制，請密切關注本站以獲得最新報名資訊。
+          </p>
+        </section>
         {/* <section>
           <h2>最新消息</h2>
         </section> */}
